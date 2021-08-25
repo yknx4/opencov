36a15920e362ded2ef1d58ddbb0e32d543fae626
Ale Ornelas <me@ale.world>
2021-08-25 17:07:58 -0500

    feat: use github content for file content

--- lib/web/controllers/file_controller.ex
+++ lib/web/controllers/file_controller.ex
@@ -9,13 +9,24 @@ defmodule Librecov.FileController do
   def show(conn, %{"id" => id}) do
     user = Authentication.get_current_account(conn)
     file = Repo.get!(File, id) |> Librecov.Repo.preload(job: [build: :project])
-    file_json = Jason.encode!(file)
 
     {:ok, auth} =
       user |> Authorizations.first_for_provider("github") |> Authorizations.ensure_fresh()
 
     Auth.with_auth_data(file.job.build.project, auth, fn auth_data ->
       {:ok, file_content} = Files.file(auth_data, file.name, file.job.build.commit_sha)
+
+      file_json =
+        file
+        |> Map.put(
+          "source",
+          file_content.content
+          |> String.split("\n")
+          |> Enum.map(&Base.decode64!/1)
+          |> Enum.join("")
+        )
+        |> Jason.encode!()
+
       render(conn, "show.html", file: file, file_json: file_json, content: file_content)
     end)
   end
