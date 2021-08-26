edbeea164c7e59b05833adb28eeeb865120bdcdc
Ale Ornelas <me@ale.world>
2021-08-25 20:54:50 -0500

    test: add test for github helper

--- lib/librecov/helpers/github.ex
+++ lib/librecov/helpers/github.ex
@@ -1,4 +1,13 @@
 defmodule Librecov.Helpers.Github do
+  @doc """
+  Github API returns errors as :ok, convert them back to error
+
+  ## Examples
+    iex> Librecov.Helpers.Github.wrap_errors({:ok, %ExOctocat.Model.BasicError{}})
+    {:error, %ExOctocat.Model.BasicError{}}
+    iex> Librecov.Helpers.Github.wrap_errors({:ok, "foo"})
+    {:ok, "foo"}
+  """
   def wrap_errors({:ok, %ExOctocat.Model.BasicError{} = e}), do: {:error, e}
   def wrap_errors(r), do: r
 end
