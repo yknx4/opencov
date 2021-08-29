001f06c7bec3cb68f2bd6334876aaaed69d73599
Ale Ornelas <me@ale.world>
2021-08-29 09:52:46 -0500

    test: change tests order

--- test/views/job_view_test.exs
+++ test/views/job_view_test.exs
@@ -3,14 +3,14 @@ defmodule Librecov.JobViewTest do
 
   import Librecov.JobView
 
-  test "job_time when run_at absent" do
-    job = insert(:job)
-    assert job_time(job) == job.inserted_at
-  end
-
   test "job_time when run_at present" do
     job = insert(:job, run_at: Timex.now())
     assert job_time(job) == job.run_at
     assert job_time(job) != job.inserted_at
+  end
+
+  test "job_time when run_at absent" do
+    job = insert(:job)
+    assert job_time(job) == job.inserted_at
   end
 end
