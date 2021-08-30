43913d694332e2d0d344119bc51241f677f9edf6
Ale Ornelas <me@ale.world>
2021-08-29 21:27:28 -0500

    test: update factories

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -32,7 +32,7 @@ defmodule Librecov.Factory do
   def build_factory do
     %Librecov.Build{
       build_number: sequence(:build_number, & &1),
-      project: build(:project)
+      project: fn -> build(:project) end
     }
   end
 
