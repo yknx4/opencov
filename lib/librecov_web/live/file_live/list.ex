61f2cfcde670948b5f1a6ca7168ae35aa38896d6
Ale Ornelas <me@ale.world>
2021-08-30 23:23:00 -0500

    style: surface format

--- lib/librecov_web/live/file_live/list.ex
+++ lib/librecov_web/live/file_live/list.ex
@@ -34,79 +34,95 @@ defmodule Librecov.FileLive.List do
     order_args = [order_field: elem(assigns.order, 0), order_direction: elem(assigns.order, 1)]
 
     ~F"""
-
     <div class="block block-rounded">
-    <div class="block-header block-header-default">
-      <h3 class="block-title">Files <span class="small">({@paginator.total_entries})</span></h3>
-      <div class="block-options">
-        <div class="dropdown">
-          <button type="button" class="btn-block-option" id="dropdown-ecom-filters" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Filters <i class="fa fa-angle-down ms-1"></i>
-          </button>
-          <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdown-ecom-filters" style="">
-            {#for {k, v} <- raw_filters()}
-              {#if Enum.any?(filters(), &(&1 == k))}
-                <Link class="dropdown-item d-flex align-items-center justify-content-between" label={v} to={apply(@path_fn, @path_args ++ [[{:filters, filters() -- [k]}|order_args]])} />
-              {#else}
-                <Link class="dropdown-item d-flex align-items-center justify-content-between" label={v} to={apply(@path_fn, @path_args ++ [[{:filters, [k|filters()]}|order_args]])}  />
-              {/if}
-            {/for}
+      <div class="block-header block-header-default">
+        <h3 class="block-title">Files <span class="small">({@paginator.total_entries})</span></h3>
+        <div class="block-options">
+          <div class="dropdown">
+            <button
+              type="button"
+              class="btn-block-option"
+              id="dropdown-ecom-filters"
+              data-bs-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Filters <i class="fa fa-angle-down ms-1" />
+            </button>
+            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdown-ecom-filters" style="">
+              {#for {k, v} <- raw_filters()}
+                {#if Enum.any?(filters(), &(&1 == k))}
+                  <Link
+                    class="dropdown-item d-flex align-items-center justify-content-between"
+                    label={v}
+                    to={apply(@path_fn, @path_args ++ [[{:filters, filters() -- [k]} | order_args]])}
+                  />
+                {#else}
+                  <Link
+                    class="dropdown-item d-flex align-items-center justify-content-between"
+                    label={v}
+                    to={apply(@path_fn, @path_args ++ [[{:filters, [k | filters()]} | order_args]])}
+                  />
+                {/if}
+              {/for}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="block-content">
+      <div class="block-content">
+        <!-- All Orders Table -->
+        <div class="table-responsive">
+          <table class="table table-borderless table-striped table-vcenter">
+            <thead>
+              <tr>
+                {#for {k, v} <- %{"coverage" => "Coverage", "diff" => "Diff", "name" => "Name"}}
+                  <th class="d-none d-sm-table-cell text-center">
+                    <Link to={apply(
+                      @path_fn,
+                      @path_args ++ [[filters: filters(), order_field: k, order_direction: row_order(@order, k)]]
+                    )}>
+                      <span>{v}</span>
+                      <Icon family="fas" icon={"sort-#{elem(@order, 1) |> sort_icon}"} :if={elem(@order, 0) == k} />
+                    </Link>
+                  </th>
+                {/for}
+              </tr>
+            </thead>
+            <tbody>
+              {#for file <- @files}
+                <tr>
+                  <td class="d-none d-sm-table-cell text-center fs-sm">{format_coverage(file.coverage)}</td>
+                  <td class="text-center fs-sm">
+                    <span class="fw-semibold">
+                      <CoverageDiff diff={file.coverage - file.previous_coverage} :if={file.previous_coverage} />
+                    </span>
+                  </td>
 
-      <!-- All Orders Table -->
-      <div class="table-responsive">
-        <table class="table table-borderless table-striped table-vcenter">
-          <thead>
-            <tr>
-              {#for {k, v} <- %{"coverage" => "Coverage", "diff" => "Diff", "name" => "Name"}}
-                <th class="d-none d-sm-table-cell text-center">
-                  <Link to={apply(@path_fn, @path_args ++ [[filters: filters(), order_field: k, order_direction: row_order(@order, k)]])}>
-                    <span>{ v }</span>
-                    <Icon family="fas" icon={"sort-#{elem(@order, 1) |> sort_icon}"} :if={elem(@order, 0) == k} />
-                  </Link>
-                </th>
+                  <td class="text-center d-none d-xl-table-cell fs-sm">
+                    <Link class="fw-semibold" label={file.name} to={Routes.file_path(@socket, :show, file)} />
+                  </td>
+                </tr>
               {/for}
-            </tr>
-          </thead>
-          <tbody>
-            {#for file <- @files}
-            <tr>
-              <td class="d-none d-sm-table-cell text-center fs-sm">{format_coverage(file.coverage)}</td>
-              <td class="text-center fs-sm">
-                <span class="fw-semibold">
-                  <CoverageDiff diff={file.coverage - file.previous_coverage} :if={file.previous_coverage} />
-                </span>
-              </td>
+            </tbody>
+          </table>
+        </div>
+        <!-- END All Orders Table -->
 
-              <td class="text-center d-none d-xl-table-cell fs-sm">
-                <Link class="fw-semibold" label={file.name} to={Routes.file_path(@socket, :show, file)} />
-              </td>
-            </tr>
-            {/for}
-          </tbody>
-        </table>
+        <!-- Pagination -->
+        <nav aria-label="Photos Search Navigation" :if={@paginator.total_pages > 1}>
+          {pagination_links(
+            @socket,
+            @paginator,
+            Enum.drop(@path_args, 2),
+            path: @path_fn,
+            action: Enum.at(@path_args, 1),
+            filters: filters(),
+            order_field: elem(@order, 0),
+            order_direction: elem(@order, 1)
+          )}
+        </nav>
+        <!-- END Pagination -->
       </div>
-      <!-- END All Orders Table -->
-
-      <!-- Pagination -->
-      <nav aria-label="Photos Search Navigation" :if={@paginator.total_pages > 1}>
-      { pagination_links @socket,
-                            @paginator,
-                            Enum.drop(@path_args, 2),
-                            path: @path_fn,
-                            action: Enum.at(@path_args, 1),
-                            filters: filters(),
-                            order_field: elem(@order, 0),
-                            order_direction: elem(@order, 1)
-    }
-
-      </nav>
-      <!-- END Pagination -->
-    </div>
     </div>
     """
   end
